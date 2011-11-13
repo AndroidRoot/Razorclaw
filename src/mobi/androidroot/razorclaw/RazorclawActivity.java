08dbd68d85cf951d5c5e8e5cc7fd597c06362dd7
Kenny Millington <kenny@kennynet.co.uk>
2011-11-13 20:23:43 +0000

    Add GNU GPLv2 headers.

--- src/mobi/androidroot/razorclaw/RazorclawActivity.java
+++ src/mobi/androidroot/razorclaw/RazorclawActivity.java
@@ -1,3 +1,22 @@
+/* RazorclawActivity.java - asus local backup root.
+ *
+ * Copyright (C) 2011 androidroot.mobi
+ *
+ * This file is part of Razorclaw.
+ * 
+ * Razorclaw is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License v2 as published by the 
+ * Free Software Foundation.
+ *
+ * Razorclaw is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package mobi.androidroot.razorclaw;
 
 import android.app.Activity;
